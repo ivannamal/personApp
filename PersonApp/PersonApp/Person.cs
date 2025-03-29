e1accd21f581525af401786dbfaec4a6928321c5
ivannamal <imalaschuk@gmail.com>
2025-03-29 23:18:10 +0200

    moved to a new location

---
+++ PersonApp/PersonApp/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApp
+{
+    internal class Person
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Email { get; }
+        public DateTime BirthDate { get; }
+
+        public bool IsAdult { get; }
+        public string SunSign { get; }
+        public string ChineseSign { get; }
+        public bool IsBirthday { get; }
+
+        public Person(string firstName, string lastName, string email, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            BirthDate = birthDate;
+
+            IsAdult = CalculateIsAdult();
+            SunSign = CalculateSunSign();
+            ChineseSign = CalculateChineseSign();
+            IsBirthday = DateTime.Today.Month == BirthDate.Month && DateTime.Today.Day == BirthDate.Day;
+        }
+
+        public Person(string firstName, string lastName, string email)
+            : this(firstName, lastName, email, DateTime.MinValue) { }
+
+        public Person(string firstName, string lastName, DateTime birthDate)
+            : this(firstName, lastName, string.Empty, birthDate) { }
+
+        private bool CalculateIsAdult()
+        {
+            var age = DateTime.Today.Year - BirthDate.Year;
+            if (BirthDate > DateTime.Today.AddYears(-age)) age--;
+            return age >= 18;
+        }
+
+        private string CalculateSunSign()
+        {
+            int day = BirthDate.Day;
+            int month = BirthDate.Month;
+            return month switch
+            {
+                1 => (day <= 19) ? "Capricorn" : "Aquarius",
+                2 => (day <= 18) ? "Aquarius" : "Pisces",
+                3 => (day <= 20) ? "Pisces" : "Aries",
+                4 => (day <= 19) ? "Aries" : "Taurus",
+                5 => (day <= 20) ? "Taurus" : "Gemini",
+                6 => (day <= 20) ? "Gemini" : "Cancer",
+                7 => (day <= 22) ? "Cancer" : "Leo",
+                8 => (day <= 22) ? "Leo" : "Virgo",
+                9 => (day <= 22) ? "Virgo" : "Libra",
+                10 => (day <= 22) ? "Libra" : "Scorpio",
+                11 => (day <= 21) ? "Scorpio" : "Sagittarius",
+                12 => (day <= 21) ? "Sagittarius" : "Capricorn",
+                _ => "Unknown"
+            };
+        }
+
+        private string CalculateChineseSign()
+        {
+            string[] signs = { "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake",
+                           "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig" };
+            return signs[(BirthDate.Year - 4) % 12];
+        }
+    }
+}
